26c5527de45e3f7e0694d8dc9db153c3a4d1d539
tae-lim <taelim92@gmail.com>
2018-08-06 12:09:53 -0700

    Implemented HoneyMakerBee.js

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.color = bee.color;
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.makeHoney = function () {
+  this.honeyPot++;
+};
+HoneyMakerBee.prototype.giveHoney = function () {
+  this.honeyPot--;
+};
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
